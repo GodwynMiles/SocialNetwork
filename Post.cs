0168bb872eeaf76c41512eee6f0d2281821c9901
spago <spago@DESKTOP-CSM61C1>
2024-01-17 14:37:00 +0800

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork
+{
+    internal class Post
+    {
+        private string text;
+
+        public Post(string text)
+        {
+            this.text = Text;
+        }
+
+        public void ShowPost()
+        {
+            Console.WriteLine("New post");
+            Console.WriteLine(text);
+        }
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                text = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialNetwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string postText = Console.ReadLine();
+
+            Post post = new Post("Hello");
+            post.Text = postText;
+            post.ShowPost();
+        }
+    }
+}
